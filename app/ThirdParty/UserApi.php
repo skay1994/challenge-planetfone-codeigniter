ec2d6ce611accf14152a32897ee30f6cae080690
Jorge Carlos <skay_1994@yahoo.com.br>
2022-12-21 11:18:49 -0300

    Feat: Create method list all users, sort and parser response

--- app/ThirdParty/UserApi.php
+++ app/ThirdParty/UserApi.php
@@ -4,6 +4,7 @@ namespace App\ThirdParty;
 
 use CodeIgniter\HTTP\CURLRequest;
 use Config\Services;
+use Exception;
 
 final class UserApi
 {
@@ -12,5 +13,28 @@ final class UserApi
     public function __construct()
     {
         $this->client = Services::curlrequest(['baseURI' => 'https://jsonplaceholder.typicode.com']);
+    }
+    public function getAll($limit = 5): array
+    {
+        try {
+            $response = $this->client->get('users', [
+                'query' => [ '_limit' => $limit ]
+            ]);
+            $body = json_decode($response->getBody());
+            usort($body, fn ($a, $b) => strcmp($a->name, $b->name));
+            return array_map([$this, 'parseResult'], $body);
+        } catch (Exception $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    private function parseResult(object $item): array
+    {
+        return [
+            'id' => $item->id,
+            'name' => $item->name,
+            'username' => $item->username,
+            'email' => $item->email,
+        ];
     }
 }
